21576ac5b00279ec1371bc881760f00abf09f5f2
michael isbell <prenezisbell@gmail.com>
2020-02-09 18:14:35 -0500

    Initial Commit

---
+++ designincode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  designincode
+//
+//  Created by michael isbell on 2/9/20.
+//  Copyright © 2020 Advanced Mobile Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
