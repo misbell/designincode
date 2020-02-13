4d9dda7d4d75e09e6b60de5d004e7e88d41da876
michael prenez-isbell <prenezisbell@gmail.com>
2020-02-13 18:57:50 -0500

    up to Modal Presentation

--- designincode/Home.swift
+++ designincode/Home.swift
@@ -10,49 +10,46 @@ import SwiftUI
 
 struct Home: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
+    
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
-            .edgesIgnoringSafeArea(.all )
-            
-            VStack {
-                HStack {
-                    Text("Watching!")
-                        .font(.system(size: 28, weight: .bold))
+                .edgesIgnoringSafeArea(.all )
+                
+            HomeView(showProfile: $showProfile)
+                .padding(.top, 44)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                     
-                    Spacer()
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                     
-                    Button(action: {self.showProfile.toggle()}) {
-                        Image("Avatar")
-                            // everything inside Button is tinted
-                            .renderingMode(.original) // removes tint
-                            .resizable()
-                            .frame(width: 36, height: 36)
-                            .clipShape(Circle())
-                    }
-                    
-                }
-                .padding(.horizontal)
-                .padding(.top, 30)
-                Spacer()
-            }
-            .padding(.top, 44)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .offset( y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
-            .scaleEffect(showProfile ? 0.9 : 1)
-            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
-            .edgesIgnoringSafeArea(.all)
+                .offset( y: showProfile ? -450 : 0)
+                .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10, y: 0, z: 0))
+                .scaleEffect(showProfile ? 0.9 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .edgesIgnoringSafeArea(.all)
             
             MenuView()
-                .offset(y: showProfile ?  0 : 600)
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ?  0 : screen.height) // use dynamic screen height
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+            }
+            .gesture (
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                }
+                .onEnded { value in
+                    if self.viewState.height > 50 {
+                        self.showProfile = false
+                    }
+                    self.viewState = .zero
+                }
+            )
         }
-        
- 
-        
     }
 }
 
@@ -61,3 +58,23 @@ struct Home_Previews: PreviewProvider {
         Home()
     }
 }
+
+struct AvatarView :View {
+    
+    // pass the variable
+    @Binding var showProfile : Bool
+    
+    var body: some View {
+        Button(action: {self.showProfile.toggle()}) {
+                               Image("Avatar")
+                                   // everything inside Button is tinted
+                                   .renderingMode(.original) // removes tint
+                                   .resizable()
+                                   .frame(width: 36, height: 36)
+                                   .clipShape(Circle())
+                           }
+    }
+}
+
+
+let screen = UIScreen.main.bounds
