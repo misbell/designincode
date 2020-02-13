28e485afbe90bc8f50577f37126af515e77e05be
michael isbell <prenezisbell@gmail.com>
2020-02-12 19:31:46 -0500

    animation between screens lesson

--- designincode/Home.swift
+++ designincode/Home.swift
@@ -9,8 +9,12 @@
 import SwiftUI
 
 struct Home: View {
+    @State var showProfile = false
     var body: some View {
         ZStack {
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+            .edgesIgnoringSafeArea(.all )
+            
             VStack {
                 HStack {
                     Text("Watching!")
@@ -18,7 +22,7 @@ struct Home: View {
                     
                     Spacer()
                     
-                    Button(action: {}) {
+                    Button(action: {self.showProfile.toggle()}) {
                         Image("Avatar")
                             // everything inside Button is tinted
                             .renderingMode(.original) // removes tint
@@ -32,7 +36,19 @@ struct Home: View {
                 .padding(.top, 30)
                 Spacer()
             }
+            .padding(.top, 44)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset( y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            
             MenuView()
+                .offset(y: showProfile ?  0 : 600)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
         }
         
  
