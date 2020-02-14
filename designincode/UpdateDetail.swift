86e2a3de2d055771eaf4fd93c11d34a78ef62ece
Michael Prenez-Isbell <prenezisbell@gmail.com>
2020-02-13 22:42:12 -0500

    second nav tutorial

---
+++ designincode/UpdateDetail.swift
@@ -0,0 +1,36 @@
+//
+//  UpdateDetail.swift
+//  designincode
+//
+//  Created by michael isbell on 2/13/20.
+//  Copyright © 2020 Advanced Mobile Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    
+    var update: Update  = updateData[0]
+    
+    var body: some View {
+        List {
+            VStack {
+                Image(update.image)
+                .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: .infinity)
+                Text(update.text)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+            }
+            .navigationBarTitle(update.title)
+        }
+        .listStyle(PlainListStyle())
+    }
+
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
